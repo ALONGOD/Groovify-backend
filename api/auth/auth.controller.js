6ce0529d0ba6fc6d4f350f0684fca4f68db27109
ALONGOD <alongoddard1@gmail.com>
2024-09-08 13:15:21 +0300

    Check commit

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,14 +1,15 @@
 import { authService } from './auth.service.js'
 import { logger } from '../../services/logger.service.js'
 
+
 export async function login(req, res) {
 	const { username, password } = req.body
 	try {
 		const user = await authService.login(username, password)
 		const loginToken = authService.getLoginToken(user)
-        
+
 		logger.info('User login: ', user)
-        
+
 		res.cookie('loginToken', loginToken, { sameSite: 'None', secure: true })
 		res.json(user)
 	} catch (err) {
@@ -23,14 +24,14 @@ export async function signup(req, res) {
 
 		// Never log passwords
 		// logger.debug(credentials)
-		
-        const account = await authService.signup(credentials)
+
+		const account = await authService.signup(credentials)
 		logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
-		
-        const user = await authService.login(credentials.username, credentials.password)
+
+		const user = await authService.login(credentials.username, credentials.password)
 		logger.info('User signup:', user)
-		
-        const loginToken = authService.getLoginToken(user)
+
+		const loginToken = authService.getLoginToken(user)
 		res.cookie('loginToken', loginToken, { sameSite: 'None', secure: true })
 		res.json(user)
 	} catch (err) {
